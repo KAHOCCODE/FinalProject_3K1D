00d04a634949d41f5c0161aba5915fcf970e671e
ka do doka <hoangkavudo@gmail.com>
2024-08-16 06:56:04 +0700

    thêm bảng giảm giá

---
+++ FinalProject_3K1D/Migrations/20240815230741_GiamGia.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinalProject_3K1D.Migrations
+{
+    /// <inheritdoc />
+    public partial class GiamGia : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+
+            migrationBuilder.CreateTable(
+                name: "KhuyenMai",
+                columns: table => new
+                {
+                    idKhuyenMai = table.Column<int>(type: "int", nullable: false),
+                    TenKhuyenMai = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: false),
+                    GiaTri = table.Column<decimal>(type: "money", nullable: false),
+                    NgayBatDau = table.Column<DateTime>(type: "datetime", nullable: false),
+                    NgayKetThuc = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KhuyenMai", x => x.idKhuyenMai);
+                });
+        }
+
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "KhuyenMai");
+        }
+    }
+}
