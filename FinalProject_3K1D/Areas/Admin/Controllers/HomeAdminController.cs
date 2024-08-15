a503d77aba9122f7536767f0cd572e2563edcb52
P1emon <khang113324@gmail.com>
2024-08-15 18:51:42 +0700

    Thêm giao diện admin

---
+++ FinalProject_3K1D/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,19 @@
+﻿using FinalProject_3K1D.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace _3K1D_Final.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeAdminController : Controller
+    {
+
+        [Route("Admin/HomeAdmin")]
+        [Route("Admin/")]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
