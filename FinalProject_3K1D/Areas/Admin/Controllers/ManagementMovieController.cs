65bd5563f263930ffd1a7758ba213549af6551b0
ka do doka <hoangkavudo@gmail.com>
2024-08-15 18:39:13 +0700

    Quản lý phim

---
+++ FinalProject_3K1D/Areas/Admin/Controllers/ManagementMovieController.cs
@@ -0,0 +1,35 @@
+﻿using FinalProject_3K1D.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace _3K1D_Final.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ManagementMovieController : Controller
+    {
+        private readonly QlrapPhimContext _context;
+
+        public ManagementMovieController(QlrapPhimContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var today =DateOnly.FromDateTime(DateTime.Now);
+            var movies = _context.Phims
+                .Where(p => p.NgayKhoiChieu <= today && p.NgayKetThuc >=today)
+                .ToList();
+            return View(movies);
+        }
+        public IActionResult MoviesEnd()
+        {
+            var today = DateOnly.FromDateTime(DateTime.Now);
+            var movies = _context.Phims
+                .Where(p => p.NgayKetThuc < today)
+                .ToList();
+            return View(movies);
+        }
+        
+
+    }
+}
