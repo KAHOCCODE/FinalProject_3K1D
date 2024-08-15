0b9225d104346aad1fe94b96a2bd07be501dcc8b
ka do doka <hoangkavudo@gmail.com>
2024-08-16 06:52:39 +0700

    Quản lý Discounts

---
+++ FinalProject_3K1D/Areas/Admin/Controllers/DiscountsController.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FinalProject_3K1D.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalProject_3K1D.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DiscountsController : Controller
+    {
+        private readonly QlrapPhimContext _context;
+
+        public DiscountsController(QlrapPhimContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Discounts
+        public async Task<IActionResult> Index()
+        {
+            var discounts = await _context.KhuyenMais.ToListAsync();
+            return View(discounts);
+        }
+
+        // GET: Admin/Discounts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Discounts/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TenKhuyenMai,GiaTri,NgayBatDau,NgayKetThuc")] KhuyenMai khuyenMai)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(khuyenMai);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(khuyenMai);
+        }
+
+        // GET: Admin/Discounts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var khuyenMai = await _context.KhuyenMais.FindAsync(id);
+            if (khuyenMai == null)
+            {
+                return NotFound();
+            }
+            return View(khuyenMai);
+        }
+
+        // POST: Admin/Discounts/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdKhuyenMai,TenKhuyenMai,GiaTri,NgayBatDau,NgayKetThuc")] KhuyenMai khuyenMai)
+        {
+            if (id != khuyenMai.IdKhuyenMai)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(khuyenMai);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!KhuyenMaiExists(khuyenMai.IdKhuyenMai))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(khuyenMai);
+        }
+
+        // GET: Admin/Discounts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var khuyenMai = await _context.KhuyenMais
+                .FirstOrDefaultAsync(m => m.IdKhuyenMai == id);
+            if (khuyenMai == null)
+            {
+                return NotFound();
+            }
+
+            return View(khuyenMai);
+        }
+
+        // GET: Admin/Discounts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var khuyenMai = await _context.KhuyenMais
+                .FirstOrDefaultAsync(m => m.IdKhuyenMai == id);
+            if (khuyenMai == null)
+            {
+                return NotFound();
+            }
+
+            return View(khuyenMai);
+        }
+
+        // POST: Admin/Discounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var khuyenMai = await _context.KhuyenMais.FindAsync(id);
+            _context.KhuyenMais.Remove(khuyenMai);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool KhuyenMaiExists(int id)
+        {
+            return _context.KhuyenMais.Any(e => e.IdKhuyenMai == id);
+        }
+    }
+}
