8f46a0cc33953007b133271f7fc5d8eee32a8dac
P1emon <khang113324@gmail.com>
2024-08-17 02:00:12 +0700

    Tạo giao diện cho Trang index của admin

--- FinalProject_3K1D/Areas/Admin/Controllers/HomeAdminController.cs
+++ FinalProject_3K1D/Areas/Admin/Controllers/HomeAdminController.cs
@@ -1,19 +1,48 @@
-﻿using FinalProject_3K1D.Models;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using System.Linq;
+using FinalProject_3K1D.Models;
 
 namespace _3K1D_Final.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class HomeAdminController : Controller
     {
+        private readonly QlrapPhimContext _context;
+
+        public HomeAdminController(QlrapPhimContext context)
+        {
+            _context = context;
+        }
 
         [Route("Admin/HomeAdmin")]
         [Route("Admin/")]
         [Route("Index")]
         public IActionResult Index()
         {
-            return View();
+            var totalCustomers = _context.KhachHangs.Count();
+            var totalEmployees = _context.NhanViens.Count();
+            var totalMovies = _context.Phims.Count();
+
+            var revenueByTheater = _context.Ves
+                .Join(_context.LichChieus,
+                      v => v.IdLichChieu,
+                      lc => lc.IdLichChieu,
+                      (v, lc) => new { v, lc })
+                .GroupBy(x => x.lc.IdRap)
+                .ToDictionary(
+                    g => g.Key,
+                    g => g.Sum(x => x.v.TienBanVe ?? 0)
+                );
+
+            var model = new DashboardViewModel
+            {
+                TotalCustomers = totalCustomers,
+                TotalEmployees = totalEmployees,
+                TotalMovies = totalMovies,
+                RevenueByTheater = revenueByTheater
+            };
+
+            return View(model);
         }
     }
 }
---
+++ FinalProject_3K1D/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace FinalProject_3K1D.Models
+{
+    public class DashboardViewModel
+    {
+        public int TotalCustomers { get; set; }
+        public int TotalEmployees { get; set; }
+        public int TotalMovies { get; set; }
+        public Dictionary<string, decimal> RevenueByTheater { get; set; }
+    }
+
+}
