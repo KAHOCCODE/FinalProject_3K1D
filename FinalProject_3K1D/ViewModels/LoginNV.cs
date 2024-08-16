1970cf4db5e96d38dbd8f80d9839211be15dd92e
ka do doka <hoangkavudo@gmail.com>
2024-08-17 01:03:38 +0700

    thêm Viewmodel  login khách hàng và nhân viên

--- FinalProject_3K1D/ViewModels/LoginNV.cs
+++ FinalProject_3K1D/ViewModels/LoginNV.cs
@@ -2,22 +2,17 @@
 
 namespace FinalProject_3K1D.ViewModels
 {
-    public class LoginKH
+    public class LoginNV
     {
         [Required(ErrorMessage = "Vui lòng nhập tên đăng nhập")]
         [Display(Name = "Username")]
-        public string UserKh { get; set; }
+        public string UserNv { get; set; }
 
         [Display(Name = "Password")]
         [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
         [DataType(DataType.Password)]
-        public string PassKh { get; set; }
+        public string PassNv { get; set; }
 
         public bool RememberMe { get; set; }
-
-        [Required(ErrorMessage = "Vui lòng chọn hình thức đăng nhập")]
-        [Display(Name = "Role")]
-        public string Role { get; set; } // New property to select the role
     }
 }
-
