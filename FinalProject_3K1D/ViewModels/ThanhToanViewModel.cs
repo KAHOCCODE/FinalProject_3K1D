86048411eab9c2167a039363a04322c23dfc3a93
Pkmax-bit <2100010779@nttu.edu.vn>
2024-08-16 13:00:53 +0700

    update view pay

---
+++ FinalProject_3K1D/ViewModels/ThanhToanViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace FinalProject_3K1D.ViewModels
+{
+    public class ThanhToanViewModel
+    {
+        public string IdVe { get; set; }
+        public DateTime NgayMua { get; set; }
+        public string TenKhachHang { get; set; }
+        public string SoDienThoai { get; set; }
+        public string TenPhim { get; set; }
+        public string MaGhe { get; set; }
+        public string TenRap { get; set; }
+        public DateTime GioChieu { get; set; }
+        public string TenPhong { get; set; }
+        public decimal TienBanVe { get; set; }
+    }
+}
