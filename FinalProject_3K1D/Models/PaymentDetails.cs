55d0978162c5c41142bdac9323c08c714d12dd3e
Pkmax-bit <2100010779@nttu.edu.vn>
2024-08-19 13:54:42 +0700

    lấy id seat và tổng tiền

---
+++ FinalProject_3K1D/Models/PaymentDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace FinalProject_3K1D.Models
+{
+    public class PaymentDetails
+    {
+        public List<string> SeatIds { get; set; }
+        public int TotalAmount { get; set; }
+    }
+}
