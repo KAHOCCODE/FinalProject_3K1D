27b2440298fcf0691372d211fdc7c2a748beadef
P1emon <khang113324@gmail.com>
2024-08-22 23:38:40 +0700

    Reapply "Thêm migration cho KhuyenMai và KhachHang"
    This reverts commit cd1f2ab5c8c8f3ff035d42d44ef3aef8906dd225.

---
+++ FinalProject_3K1D/Migrations/20240822160840_AddIdKhuyenMaiToKhachHang.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinalProject_3K1D.Migrations
+{
+    public partial class AddIdKhuyenMaiToKhachHang : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Thêm cột idKhuyenMai vào bảng KhachHang
+            migrationBuilder.AddColumn<int>(
+                name: "idKhuyenMai",
+                table: "KhachHang",
+                type: "int",
+                nullable: true);
+
+            // Tạo chỉ mục cho cột idKhuyenMai
+            migrationBuilder.CreateIndex(
+                name: "IX_KhachHang_idKhuyenMai",
+                table: "KhachHang",
+                column: "idKhuyenMai");
+
+            // Thêm khóa ngoại cho cột idKhuyenMai, liên kết đến bảng KhuyenMai
+            migrationBuilder.AddForeignKey(
+                name: "FK_KhachHang_KhuyenMai_idKhuyenMai",
+                table: "KhachHang",
+                column: "idKhuyenMai",
+                principalTable: "KhuyenMai",
+                principalColumn: "idKhuyenMai",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Xóa khóa ngoại nếu có
+            migrationBuilder.DropForeignKey(
+                name: "FK_KhachHang_KhuyenMai_idKhuyenMai",
+                table: "KhachHang");
+
+            // Xóa chỉ mục nếu có
+            migrationBuilder.DropIndex(
+                name: "IX_KhachHang_idKhuyenMai",
+                table: "KhachHang");
+
+            // Xóa cột idKhuyenMai khỏi bảng KhachHang
+            migrationBuilder.DropColumn(
+                name: "idKhuyenMai",
+                table: "KhachHang");
+        }
+    }
+}
