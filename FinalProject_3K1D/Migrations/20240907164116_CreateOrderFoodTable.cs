e6630977f73a07fe5ba56eb40804d5c0e9c6e3cb
Pkmax-bit <2100010779@nttu.edu.vn>
2024-09-07 23:46:20 +0700

    update database OrderFood

---
+++ FinalProject_3K1D/Migrations/20240907164116_CreateOrderFoodTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinalProject_3K1D.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateOrderFoodTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderFoods",
+                columns: table => new
+                {
+                    IdSanPham = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TenSanPham = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gia = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    MoTa = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apphich = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PLoai = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderFoods", x => x.IdSanPham);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderFoods");
+        }
+    }
+}
