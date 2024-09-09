77711374ce216a9a7afb668a11bc704c02a88c5a
ka do doka <hoangkavudo@gmail.com>
2024-09-09 13:54:37 +0700

    Xóa Models Orders và OderFoods

--- FinalProject_3K1D/Models/QlrapPhimContext.cs
+++ FinalProject_3K1D/Models/QlrapPhimContext.cs
@@ -34,15 +34,13 @@ public partial class QlrapPhimContext : DbContext
     public virtual DbSet<Ve> Ves { get; set; }
 
     public virtual DbSet<KhuyenMai> KhuyenMais { get; set; }
-    public DbSet<Order> Orders { get; set; }
     public virtual DbSet<DanhGia> DanhGias { get; set; }
     public virtual DbSet<GiaHoan> GiaHoans { get; set; }
-    public virtual DbSet<OrderFood> OrderFoods { get; set; }
     public virtual DbSet<Food> Foods { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-0U55SOV\\SQLEXPRESS;Initial Catalog=QLRapPhim;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-3TRF427\\HOANGKA;Initial Catalog=QLRapPhim;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
