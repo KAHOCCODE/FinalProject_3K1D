b7cb38668e052879f1b5191bc30b8c1c03658550
ka do doka <hoangkavudo@gmail.com>
2024-09-09 13:55:49 +0700

    Thêm Magration để xóa bảng Oders và OderFoods

--- FinalProject_3K1D/Migrations/20240909065243_DeleteTableOrdersAndOderFoods.Designer.cs
+++ FinalProject_3K1D/Migrations/20240909065243_DeleteTableOrdersAndOderFoods.Designer.cs
@@ -4,6 +4,7 @@ using FinalProject_3K1D.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FinalProject_3K1D.Migrations
 {
     [DbContext(typeof(QlrapPhimContext))]
-    partial class QlrapPhimContextModelSnapshot : ModelSnapshot
+    [Migration("20240909065243_DeleteTableOrdersAndOderFoods")]
+    partial class DeleteTableOrdersAndOderFoods
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FinalProject_3K1D/Migrations/20240909065243_DeleteTableOrdersAndOderFoods.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinalProject_3K1D.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteTableOrdersAndOderFoods : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderFoods");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderFoods",
+                columns: table => new
+                {
+                    IdSanPham = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Apphich = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gia = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    MoTa = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PLoai = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TenSanPham = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderFoods", x => x.IdSanPham);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Idsanpham = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Apphich = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Mota = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PLoai = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tensanpham = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Idsanpham);
+                });
+        }
+    }
+}
