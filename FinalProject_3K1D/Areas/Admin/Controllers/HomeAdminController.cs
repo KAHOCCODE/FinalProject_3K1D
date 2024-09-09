31b41176fac67b4b6dde36f33ffc38f69d6be5b0
P1emon <khang113324@gmail.com>
2024-09-09 16:02:05 +0700

    Thêm lại giao diện của admin
    Giao diện của phần lớn quản lý

--- FinalProject_3K1D/Areas/Admin/Controllers/HomeAdminController.cs
+++ FinalProject_3K1D/Areas/Admin/Controllers/HomeAdminController.cs
@@ -23,23 +23,61 @@ namespace _3K1D_Final.Areas.Admin.Controllers
             var totalEmployees = _context.NhanViens.Count();
             var totalMovies = _context.Phims.Count();
 
+            // Doanh thu theo tên rạp
             var revenueByTheater = _context.Ves
                 .Join(_context.LichChieus,
                       v => v.IdLichChieu,
                       lc => lc.IdLichChieu,
                       (v, lc) => new { v, lc })
-                .GroupBy(x => x.lc.IdRap)
+                .Join(_context.Raps,
+                      lc => lc.lc.IdRap,
+                      r => r.IdRap,
+                      (lc, r) => new { lc.v, lc.lc, r })
+                .GroupBy(x => x.r.TenRap)
                 .ToDictionary(
                     g => g.Key,
                     g => g.Sum(x => x.v.TienBanVe ?? 0)
                 );
+
+            // Top 3 phim có doanh thu cao nhất theo từng rạp
+            var topMoviesByTheater = _context.Ves
+                .Join(_context.LichChieus,
+                      v => v.IdLichChieu,
+                      lc => lc.IdLichChieu,
+                      (v, lc) => new { v, lc })
+                .Join(_context.Raps,
+                      lc => lc.lc.IdRap,
+                      r => r.IdRap,
+                      (lc, r) => new { lc.v, lc.lc, r })
+                .GroupBy(x => new { x.r.TenRap, x.lc.IdPhim })
+                .Select(g => new
+                {
+                    TheaterName = g.Key.TenRap,
+                    MovieTitle = _context.Phims.FirstOrDefault(p => p.IdPhim == g.Key.IdPhim).TenPhim,
+                    TotalRevenue = g.Sum(x => x.v.TienBanVe ?? 0)
+                })
+                .GroupBy(x => x.TheaterName)
+                .Select(g => new DashboardViewModel.TheaterTopMovies
+                {
+                    TheaterName = g.Key,
+                    TopMovies = g.OrderByDescending(x => x.TotalRevenue)
+                                 .Take(3)
+                                 .Select(x => new DashboardViewModel.MovieRevenue
+                                 {
+                                     MovieTitle = x.MovieTitle,
+                                     TotalRevenue = x.TotalRevenue
+                                 })
+                                 .ToList()
+                })
+                .ToList();
 
             var model = new DashboardViewModel
             {
                 TotalCustomers = totalCustomers,
                 TotalEmployees = totalEmployees,
                 TotalMovies = totalMovies,
-                RevenueByTheater = revenueByTheater
+                RevenueByTheater = revenueByTheater,
+                TopMoviesByTheater = topMoviesByTheater
             };
 
             return View(model);
--- FinalProject_3K1D/ViewModels/DashboardViewModel.cs
+++ FinalProject_3K1D/ViewModels/DashboardViewModel.cs
@@ -8,6 +8,20 @@ namespace FinalProject_3K1D.Models
         public int TotalEmployees { get; set; }
         public int TotalMovies { get; set; }
         public Dictionary<string, decimal> RevenueByTheater { get; set; }
-    }
 
+        // Thêm thuộc tính mới
+        public List<TheaterTopMovies> TopMoviesByTheater { get; set; }
+
+        public class MovieRevenue
+        {
+            public string MovieTitle { get; set; }
+            public decimal TotalRevenue { get; set; }
+        }
+
+        public class TheaterTopMovies
+        {
+            public string TheaterName { get; set; }
+            public List<MovieRevenue> TopMovies { get; set; }
+        }
+    }
 }
