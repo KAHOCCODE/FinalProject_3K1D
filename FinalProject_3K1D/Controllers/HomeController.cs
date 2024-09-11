0726673ef51023bd8ad40315685878460a5bb22b
ka do doka <hoangkavudo@gmail.com>
2024-09-11 15:24:57 +0700

    Update lại hiển thị đánh giá phim (tui hận KHOA)

--- FinalProject_3K1D/Controllers/HomeController.cs
+++ FinalProject_3K1D/Controllers/HomeController.cs
@@ -69,6 +69,7 @@ namespace FinalProject_3K1D.Controllers
                         .ThenInclude(l => l.IdPhongChieuNavigation)
                     .Include(p => p.LichChieus)
                         .ThenInclude(l => l.IdRapNavigation)
+                    .Include(p => p.DanhGias)
                     .FirstOrDefault(p => p.IdPhim == id);
 
                 if (phim == null)
@@ -76,9 +77,17 @@ namespace FinalProject_3K1D.Controllers
                     return NotFound(); // Handle case where movie is not found
                 }
 
+                // Calculate average rating
+                var averageRating = phim.DanhGias.Any()
+                    ? phim.DanhGias.Average(dg => dg.Diem)
+                    : 0;
+
+                ViewBag.AverageRating = averageRating;
+
                 return View(phim);
             }
         }
+
 
         [HttpGet]
         public IActionResult GetShowtimes(string cinemaId)
